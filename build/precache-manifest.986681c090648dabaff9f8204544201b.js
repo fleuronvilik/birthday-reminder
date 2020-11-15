436944d854f0ca8c6db0c818d6185cce483b68c6
Fleuron Gnadekpa <vilfleur@gmail.com>
2020-11-16 00:47:20 +0100

    Add build folder

---
+++ build/precache-manifest.986681c090648dabaff9f8204544201b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606134c3d9504bfda72963026a5192f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fd89da7c3cb2c2ea0b0",
+    "url": "/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd89da7c3cb2c2ea0b0",
+    "url": "/static/js/main.10802f73.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
